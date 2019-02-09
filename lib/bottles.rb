ec85043b9377ff050c93c097bd453a9fb6c3d344
Abilio Almeida Eiroa <bili@nextail.co>
2019-02-09 11:36:28 +0100

    Extracting sentences from verse

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,13 +1,22 @@
 class Bottles
   def verse(count)
-    verse = <<-VERSE
-#{bottles(count)} of beer on the wall, #{bottles(count)} of beer.
-#{take_down(count)} and pass it around, #{bottles(count - 1)} of beer on the wall.
-VERSE
-    verse
+    [first_sentence(count), second_sentence(count)].join()
   end
 
   private
+
+  def first_sentence(count)
+    "#{bottles(count).capitalize} of beer on the wall, #{bottles(count)} of beer.\n"
+  end
+
+  def second_sentence(count)
+    case count
+    when 0
+      "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+    else
+      "#{take_down(count)} and pass it around, #{bottles(count - 1)} of beer on the wall.\n"
+    end
+  end
 
   def bottles(count)
     case count
