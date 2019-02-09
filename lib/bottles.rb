448ce16d76ce0f7c7285a5f6d889dc7a617ded27
Abilio Almeida Eiroa <bili@nextail.co>
2019-02-09 11:05:30 +0100

    extracting the bottles method to pluralize

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,9 +1,20 @@
 class Bottles
   def verse(count)
     verse = <<-VERSE
-#{count} bottles of beer on the wall, #{count} bottles of beer.
-Take one down and pass it around, #{count - 1} bottles of beer on the wall.
+#{bottles(count)} of beer on the wall, #{bottles(count)} of beer.
+Take one down and pass it around, #{bottles(count - 1)} of beer on the wall.
 VERSE
     verse
+  end
+
+  private
+
+  def bottles(count)
+    case count
+    when 1
+      "#{count} bottle"
+    else
+      "#{count} bottles"
+    end
   end
 end
