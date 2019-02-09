5ee9f1c372f4ae5e0fa84e595fae072d0e5add35
Abilio Almeida Eiroa <bili@nextail.co>
2019-02-09 11:12:34 +0100

    Extracting take_down method

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -2,7 +2,7 @@ class Bottles
   def verse(count)
     verse = <<-VERSE
 #{bottles(count)} of beer on the wall, #{bottles(count)} of beer.
-Take one down and pass it around, #{bottles(count - 1)} of beer on the wall.
+#{take_down(count)} and pass it around, #{bottles(count - 1)} of beer on the wall.
 VERSE
     verse
   end
@@ -11,10 +11,21 @@ VERSE
 
   def bottles(count)
     case count
+    when 0
+      "no more bottles"
     when 1
       "#{count} bottle"
     else
       "#{count} bottles"
+    end
+  end
+
+  def take_down(count)
+    case count
+    when 1
+      "Take it down"
+    else
+      "Take one down"
     end
   end
 end
